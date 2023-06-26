a884084f7c2ded4c87b5574da2d8c6d3b293185e
MDandersonm <1215kimyj@gmail.com>
2023-06-26 23:51:32 +0900

    feat: [김영진] 메타코딩-스프링부트 시큐리티- facebook oauth [META-SECURITY-XX]
    스프링부트 시큐리티 10강 - 페이스북 로그인 완료

--- security1/src/main/java/com/cos/securityex01/config/oauth/PrincipalOauth2UserService.java
+++ security1/src/main/java/com/cos/securityex01/config/oauth/PrincipalOauth2UserService.java
@@ -59,11 +59,30 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 		System.out.println("oAuth2User.getAttributes() : " + oAuth2User.getAttributes());
 		
 		//회원가입을 강제로진행
-		String provider = userRequest.getClientRegistration().getClientId();//google
-		String providerId= oAuth2User.getAttribute("sub");
+		OAuth2UserInfo oAuth2UserInfo = null;
+		if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+			System.out.println("구글 로그인 요청~~");
+			oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+			
+		}else if(userRequest.getClientRegistration().getRegistrationId().equals("facebook")) {
+			System.out.println("페이스북 로그인 요청~~");
+			oAuth2UserInfo = new FaceBookUserInfo(oAuth2User.getAttributes());
+		}else {
+			System.out.println("우리는 구글과 페이스북만 지원합니다.");
+		}
+		
+
+		
+		
+//		String provider = userRequest.getClientRegistration().getRegistrationId();//google  .  구글만 사용할경우 이렇게 씀
+		String provider = oAuth2UserInfo.getProvider();
+		System.out.println("provider: "+provider);
+//		String providerId= oAuth2User.getAttribute("sub");
+		String providerId= oAuth2UserInfo.getProviderId();
 		String username = provider+"_" +providerId;
 		String password = bCryptPasswordEncoder.encode("겟인데어");
-		String email= oAuth2User.getAttribute("email");
+//		String email= oAuth2User.getAttribute("email");
+		String email= oAuth2UserInfo.getEmail();
 		String role= "Role_USER";
 		//아이디 이미 회원가입되어있는지 확인절차
 		User userEntity =userRepository.findByUsername(username);
@@ -78,11 +97,15 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 					.build();
 			userRepository.save(userEntity);
 		}
+			
+		
 		
 //		return super.loadUser(userRequest);
 		return new PrincipalDetails(userEntity,oAuth2User.getAttributes());//이게 만들어져서 Athuntication객체에 들어감.
 //		return processOAuth2User(userRequest, oAuth2User);
-	}
+		}
+		
+		
 //
 //	private OAuth2User processOAuth2User(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
 //
